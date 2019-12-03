5639cd0fc9f66d9e1064266a86532383025664d5
Justin Liew <jliew@fastly.com>
2019-12-02 22:30:41 -0800

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,35 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
+fn run(mut program: Vec<i32>, noun: i32, verb: i32) -> bool {
+    let mut pc = 0;
+    program[1] = noun;
+    program[2] = verb;
+    while pc < program.len() {
+        let (i,v) = match program[pc] {
+            1 => {
+                (program[pc+3],program[program[pc+1] as usize]+program[program[pc+2] as usize])
+            }
+            2 => {
+                (program[pc+3],program[program[pc+1] as usize]*program[program[pc+2] as usize])
+            }
+            99 => {
+                (-1,0)
+            }
+            _ => {
+                (-1,0)
+            }
+        };
+        if i >= 0 {
+            program[i as usize] = v;
+            pc += 4;
+        } else {
+            break;
+        }
+    }
+    program[0] == 19690720
+}
+
 fn main() {
     let file = File::open("input.txt").expect("Unable to open file input.txt");
     let reader = BufReader::new(file);
@@ -17,31 +46,12 @@ fn main() {
             program.push(op);
         }
 
-        let mut pc = 0;
-        while pc < program.len() {
-            println!("Op: {}", program[pc]);
-            let (i,v) = match program[pc] {
-                1 => {
-                    (program[pc+3],program[program[pc+1] as usize]+program[program[pc+2] as usize])
+        for n in 0..99 {
+            for v in 0..99 {
+                if run(program.clone(),n,v) {
+                    println!("noun: {}, verb: {} = {}", n,v, n*100+v);
                 }
-                2 => {
-                    (program[pc+3],program[program[pc+1] as usize]*program[program[pc+2] as usize])
-                }
-                99 => {
-                    (-1,0)
-                }
-                _ => {
-                    (-1,0)
-                }
-            };
-            println!("i,v: {},{}", i,v);
-            if i >= 0 {
-                program[i as usize] = v;
-                pc += 4;
-            } else {
-                break;
             }
         }
-        println!("{}", program[0]);
     }
 }
