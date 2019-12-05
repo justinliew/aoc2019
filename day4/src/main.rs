1d33624c2f6db4da069c381fd1309d84c4c67f9c
Justin Liew <jliew@fastly.com>
2019-12-05 10:37:31 -0800

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,18 +12,39 @@ fn main() {
     let mut total = 0;
     for value in lower..=upper {
         let digits = number_to_vec(value);
+        let num_digits = digits.len();
         let mut last = digits[0];
         let mut valid = false;
-        for i in 1..digits.len() {
-            if digits[i] == last {
-                valid = true;
-            }
+        for i in 0..num_digits {
             if digits[i] < last {
                 valid = false;
                 break;
             }
+
+            let found = match i {
+                0 => {
+                    digits[0] == digits[1] &&
+                    digits[1] != digits[2]
+                },
+                4 => {
+                    digits[4] == digits[5] &&
+                    digits[4] != digits[3]
+                }
+                5 => {
+                    false
+                },
+                _ => {
+                    digits[i] == digits[i+1] &&
+                    digits[i-1] != digits[i] &&
+                    digits[i+1] != digits[i+2]
+                }
+            };
+            if found {
+                valid = true;
+            }
             last = digits[i];
         }
+
         if valid {
             total += 1;
         }
