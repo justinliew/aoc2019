b2000a748b5dba2d9b42c08b1bd6234f1be850a4
Justin Liew <jliew@fastly.com>
2019-12-04 21:42:19 -0800

    day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,6 +3,33 @@ use std::io::{prelude::*, BufReader};
 
 type Point = (i32,i32);
 
+fn length(a: &[Point]) -> i32 {
+    (a[0].0 - a[1].0).abs() +
+    (a[0].1 - a[1].1).abs()
+}
+
+fn traverse(a: &[Point], b: &[Point], aindex: usize, bindex: usize) -> i32 {
+    let mut total = 0;
+    for ai in 0..aindex {
+        let len = length(&a[ai..=(ai+1)]);
+        total += len;
+    }
+    for bi in 0..bindex {
+        let len = length(&b[bi..=(bi+1)]);
+        total += len;
+    }
+    let (valid,point) = intersects((a[aindex],a[aindex+1]),(b[bindex],b[bindex+1]));
+    total + match valid {
+        true => {
+            length(&vec![a[aindex],point]) +
+            length(&vec![b[bindex],point])
+        },
+        false => {
+            0
+        }
+    }
+}
+
 #[derive(PartialEq,Debug)]
 enum Dir {
     Vertical,
@@ -35,7 +62,8 @@ fn intersects(a: (Point,Point), b: (Point,Point)) -> (bool,Point) {
     let inter = (a_dir.1 >= bl && a_dir.1 <= bg) &&
                 (b_dir.1 >= al && b_dir.1 <= ag) &&
                 (a_dir.1 != 0 || al != 0 || ag != 0) &&
-                (b_dir.1 != 0 || bl != 0 || bg != 0);
+                (b_dir.1 != 0 || bl != 0 || bg != 0) &&
+                (a_dir.1.abs() != 0 && b_dir.1.abs() != 0);
     match a_dir.0 {
         Dir::Vertical => {
             (inter,(a_dir.1,b_dir.1))
@@ -90,11 +118,18 @@ fn main() {
     let mut smallest = std::i32::MAX;
     for i in 0..segs[0].len()-1 {
         for j in 0..segs[1].len()-1 {
-            let (valid,point) = intersects((segs[0][i],segs[0][i+1]),(segs[1][j],segs[1][j+1]));
+            let (valid,_) = intersects((segs[0][i],segs[0][i+1]),(segs[1][j],segs[1][j+1]));
             if valid {
-                let dist = point.0.abs() + point.1.abs();
+                // part 1
+                // let dist = point.0.abs() + point.1.abs();
+                // if dist != 0 && dist < smallest {
+                //     smallest = point.0.abs() + point.1.abs();
+                // }
+
+                // part 2
+                let dist = traverse(&segs[0], &segs[1],i,j);
                 if dist != 0 && dist < smallest {
-                    smallest = point.0.abs() + point.1.abs();
+                    smallest = dist;
                 }
             }
         }
