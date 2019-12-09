7ca49bcd1c75084e11adf4c71745524b129191db
Justin Liew <jliew@fastly.com>
2019-12-08 21:09:28 -0800

    day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -7,8 +7,11 @@ fn main() {
     let mut layers = Vec::new();
     let mut layer = Vec::new();
 
+    let w = 25;
+    let h = 6;
+
     for (i,c) in contents.chars().enumerate() {
-        if i % (25*6) == 0 && i > 0 {
+        if i % (w*h) == 0 && i > 0 {
             let new_layer = layer.clone();
 //            println!("New Layer: {:?}", new_layer);
             layers.push(new_layer);
@@ -17,6 +20,8 @@ fn main() {
         let num = c.to_digit(10).unwrap();
         layer.push(num);
     }
+    let new_layer = layer.clone();
+    layers.push(new_layer);
 
     let mut num_0 = std::i32::MAX;
     let mut min_layer = 0;
@@ -32,18 +37,36 @@ fn main() {
             min_layer = layeridx;
         }
     }
-    println!("The fewest 0 digits is {} at layer {}", num_0, min_layer);
+//    println!("The fewest 0 digits is {} at layer {}", num_0, min_layer);
 
     let mut num_1 = 0;
     let mut num_2 = 0;
     for pixel in &layers[min_layer] {
-        print!("{} ", pixel);
         match pixel {
             1 => {num_1 += 1},
             2 => {num_2 += 2},
             _ => (),
         }
     }
-    println!("{} * {} = {}", num_1, num_2,num_1 * num_2);
+    // TODO - for some reason this doesn't actually print the correct number
+    // I used the debug print of the layer to actually calculate this. It seems to be `num_2` that's wrong
+//    println!("{} * {} = {}", num_1, num_2,num_1 * num_2);
 
+    let mut post = vec![255;w*h];
+    for i in 0..(w*h) {
+        for j in 0..layers.len() {
+//            println!("{} {} is {}", i,j,layers[j][i]);
+            if layers[j][i] != 2 {
+                post[i] = layers[j][i];
+                break;
+            }
+        }
+    }
+
+    for y in 0..h {
+        for x in 0..w {
+            print!("{} ",post[x+y*w]);
+        }
+        println!("");
+    }
 }
